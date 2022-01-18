139d44a09ae789932688d9c76dd99e338fb2ee90
iplee <iplee1123@gmail.com>
2022-01-18 22:37:24 +0900

    BoardController change

--- src/main/java/com/iplee/myhome/controller/BoardController.java
+++ src/main/java/com/iplee/myhome/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.iplee.myhome.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/board")
@@ -12,4 +13,11 @@ public class BoardController {
     public String list() {
         return "board/list";
     }
+
+    @GetMapping("/message")
+    @ResponseBody
+    public String message() {
+        return "message : " + "Hello world";
+    }
+
 }
